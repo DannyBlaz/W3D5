94b7cb6dea8c2bda072579e78e52912b11cc1a4c
Daniel <dannyblaze469@gmail.com>
2021-03-12 16:35:41 -0500

    Created parents=, add_child, remove_child

--- PolyTreeNode/lib/00_tree_node.rb
+++ PolyTreeNode/lib/00_tree_node.rb
@@ -6,10 +6,27 @@ class PolyTreeNode
         @children = children
     end
 
-    def parent=
-        
+    def parent=(new_parent)
+        if parent
+            parent.children.delete(self)
+        end
+        @parent = new_parent
+        if !(self.parent.nil?) && !(new_parent.children.include?(self))
+            new_parent.children << self #unless self.parent.nil?
+        end
+            
     end
 
+    def add_child(child)
+        child.parent = self
+    end
+
+    def remove_child(child)
+        if !self.children.include?(child)
+            raise "node is not a child"
+        end
+        child.parent = nil
+    end
 
 
 end
