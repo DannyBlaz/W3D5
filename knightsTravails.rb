b7022933167eceb2d8dae2ed0be29408929dd14e
Daniel <dannyblaze469@gmail.com>
2021-03-15 14:37:43 -0400

    Build move tree

--- knightsTravails.rb
+++ knightsTravails.rb
@@ -1,9 +1,17 @@
 class KnightPathFinder
+    attr_accessor :root_node
+    attr_reader :start_pos
+
+    def self.valid_moves(pos)
+        valid_mov = []
+
+    end
 
     def initialize(start_pos)
         @size = Array.new(8) {Array.new(8,[])}
         @start_pos = start_pos
-
+        @conciderd_pos = [start_pos]
+        build_move_three
     end
 
     def self.root_node
@@ -11,6 +19,6 @@ class KnightPathFinder
     end
 
     def build_move_three
-
+        self.root_node = PolyTreeNode(start_pos)
     end
 end
